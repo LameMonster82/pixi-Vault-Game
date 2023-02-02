0989e3646829997e41ca2b387343251cafba910c
LlamasHere <llamashere@posteo.de>
2023-02-02 16:40:37 +0200

    feat!: First fully featured version of the Vault Game

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import {Application, Assets, Container, FederatedPointerEvent, Rectangle, Sprite, Text, TextStyle,} from 'pixi.js'
+import * as PIXI from 'pixi.js';
+import {gsap} from "gsap";
+import {PixiPlugin} from "gsap/PixiPlugin";
 
-const app: Application = new Application({
+gsap.registerPlugin(PixiPlugin);
+PixiPlugin.registerPIXI(PIXI);
+
+const app: PIXI.Application = new PIXI.Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
@@ -16,27 +21,30 @@ enum KeyDirections {
 interface keyCombo {
 	secretKey: number;
 	direction: KeyDirections;
+	rotation: number;
+	tweenAnim?: gsap.core.Tween;
 }
 
 
 
-const backgroundContainer = new Container();
+const backgroundContainer = new PIXI.Container();
 app.stage.addChild(backgroundContainer);
-let handleSprite: Sprite;
-const secretWheels: Container[] = [];
-const handleContainer = new Container();
+let handleSprite: PIXI.Sprite;
+const secretWheels: PIXI.Container[] = [];
+const handleContainer = new PIXI.Container();
 app.stage.addChild(handleContainer);
-const endScreen = new Container();
+const endScreen = new PIXI.Container();
 let wheelRadius = 120;
 let wheelIndex = 0;
 const keyComboCount = 3;
 let keyComboAnswer: keyCombo[] = CreateRandomKeyCombo(keyComboCount);
 let secretWheelCombo: keyCombo[] = [
-	{secretKey: 1, direction: KeyDirections.Left},
-	{secretKey: 1, direction: KeyDirections.Left},
-	{secretKey: 1, direction: KeyDirections.Left},
-
+	{secretKey: 1, direction: KeyDirections.Left, rotation: 0},
+	{secretKey: 1, direction: KeyDirections.Left, rotation: 0},
+	{secretKey: 1, direction: KeyDirections.Left, rotation: 0},
 ]
+let endGameTimeoutSeconds = 0;
+let timer = 0;
 
 console.log(keyComboAnswer);
 
@@ -45,16 +53,18 @@ function CreateRandomKeyCombo(keyComboLenght: number): keyCombo[]  {
 	for (let i = 0; i < keyComboLenght; i++) {
 		let randomCombo: keyCombo = {
 			secretKey: Math.floor(Math.random() * 9) + 1,
-			direction: Math.round(Math.random()) == 0 ? KeyDirections.Left : KeyDirections.Right};
+			direction: Math.round(Math.random()) == 0 ? KeyDirections.Left : KeyDirections.Right,
+			rotation: 0,
+		};
 		answer.push(randomCombo)
 	}
 
 	return answer;
 }
 
-function loadImageToContainer(image: string, xoffset: number, yoffset: number, scale: number, container: Container): Sprite {
+function loadImageToContainer(image: string, xoffset: number, yoffset: number, scale: number, container: PIXI.Container): PIXI.Sprite {
 	//console.log(image)
-	let imageSprite: Sprite = Sprite.from(image);
+	let imageSprite: PIXI.Sprite = PIXI.Sprite.from(image);
 	imageSprite.position.set(app.screen.width/2 + xoffset, app.screen.height/2 + yoffset );
 	imageSprite.anchor.set(0.5);
 	imageSprite.scale.set(
@@ -68,7 +78,7 @@ function loadImageToContainer(image: string, xoffset: number, yoffset: number, s
 	return imageSprite;
 }
 
-const style = new TextStyle({
+const style = new PIXI.TextStyle({
 	fontFamily: 'Arial',
 	fontSize: 56,
 	fontStyle: 'italic',
@@ -88,13 +98,13 @@ const style = new TextStyle({
 });
 
 
-function createPasscodeWheels(radius: number, x: number, y: number): Container {
-	let newWheel: Container = new Container();
+function createPasscodeWheels(radius: number, x: number, y: number): PIXI.Container {
+	let newWheel: PIXI.Container = new PIXI.Container();
 	newWheel.pivot.set(window.innerWidth / 2, window.innerHeight / 2);
 	newWheel.position.set(x, y);
 
 	for (let i = 0; i < 9; i++) {
-		let text = new Text(i + 1, style);
+		let text = new PIXI.Text(i + 1, style);
 		let radiant = (40 * i + 260) * Math.PI / 180;
 		let xcord = radius * Math.cos(radiant);
 		let ycord = radius * Math.sin(radiant);
@@ -110,48 +120,51 @@ function createPasscodeWheels(radius: number, x: number, y: number): Container {
 	return newWheel;
 }
 
-function rotateHandle(event: FederatedPointerEvent) {
+function rotateHandle(event: PIXI.FederatedPointerEvent) {
 	if(event.buttons === 1 ) {
+		if(secretWheelCombo[wheelIndex].tweenAnim != undefined) {
+			// @ts-ignore
+			secretWheelCombo[wheelIndex].tweenAnim.kill();
+		}
+
 		let newRotation = (Math.PI / 180) * event.movementX;
 		secretWheels[wheelIndex].children[0].rotation += newRotation;
+		secretWheelCombo[wheelIndex].rotation += newRotation;
+
 
 		if(newRotation > 0) {
-			let shieldSprite = secretWheels[wheelIndex].children[1] as Sprite;
-			shieldSprite.texture = Assets.get("wheelShieldRight");
+			let shieldSprite = secretWheels[wheelIndex].children[1] as PIXI.Sprite;
+			shieldSprite.texture = PIXI.Assets.get("wheelShieldRight");
 			secretWheelCombo[wheelIndex].direction = KeyDirections.Right;
 		} else if (newRotation < 0) {
-			let shieldSprite = secretWheels[wheelIndex].children[1] as Sprite;
-			shieldSprite.texture = Assets.get("wheelShieldLeft");
+			let shieldSprite = secretWheels[wheelIndex].children[1] as PIXI.Sprite;
+			shieldSprite.texture = PIXI.Assets.get("wheelShieldLeft");
 			secretWheelCombo[wheelIndex].direction = KeyDirections.Left;
 		}
 
 		handleSprite.rotation += newRotation;
+
 	} else {
-		let degrees = secretWheels[wheelIndex].children[0].rotation * 180/Math.PI;
-		let newDegrees = closestNumber(degrees, 40);
-		handleSprite.rotation = (Math.PI / 180) * newDegrees;
-		if(newDegrees > 0) {
-			newDegrees = -(360 - newDegrees);
-		}
-		secretWheels[wheelIndex].children[0].rotation = (Math.PI / 180) * newDegrees;
-
-		let key = ((Math.abs(degrees) + 40) / 40) - roundDown((Math.abs(degrees) + 40) / 40, 9);
-		if(key === 0)
-			key = 9; //Edge case
-
-		secretWheelCombo[wheelIndex].secretKey = Math.round(key);
-		console.log(newDegrees + " " + key);
+		finishRotatingWheels(wheelIndex);
 	}
 }
 
 function finishRotatingWheels(index: number) {
-	let degrees = secretWheels[index].children[0].rotation * 180/Math.PI;
+	let degrees = secretWheelCombo[index].rotation * 180 / Math.PI;
+
 	let newDegrees = closestNumber(degrees, 40);
 	handleSprite.rotation = (Math.PI / 180) * newDegrees;
 	if(newDegrees > 0) {
 		newDegrees = -(360 - newDegrees);
 	}
-	secretWheels[index].children[0].rotation = (Math.PI / 180) * newDegrees;
+	if(secretWheelCombo[index].tweenAnim != undefined) {
+		// @ts-ignore
+		secretWheelCombo[index].tweenAnim.kill();
+	}
+
+	secretWheelCombo[index].tweenAnim = gsap.to(secretWheels[index].children[0] as PIXI.Sprite, {pixi:{rotation: newDegrees}, duration: 1});
+	console.log(degrees);
+	//secretWheels[index].children[0].rotation = (Math.PI / 180) * newDegrees;
 
 	let key = ((Math.abs(degrees) + 40) / 40) - roundDown((Math.abs(degrees) + 40) / 40, 9);
 	if(key === 0)
@@ -173,8 +186,6 @@ function checkCode(): boolean {
 		if(secretWheelCombo[i].secretKey != keyComboAnswer[i].secretKey ||
 			secretWheelCombo[i].direction != keyComboAnswer[i].direction) {
 			console.log("WRONG " + i);
-
-			console.log(keyComboAnswer);
 			return false;
 		}
 	}
@@ -188,40 +199,82 @@ async function openVault() {
 	}
 
 	if (checkCode() || true) {
-		let door = backgroundContainer.children.find(value => value.name === "doorClosed") as Sprite;
-		door.texture = await Assets.load("doorOpened");
+		let door = backgroundContainer.children.find(value => value.name === "doorClosed") as PIXI.Sprite;
+		door.texture = await PIXI.Assets.load("doorOpened");
 		door.position.set(window.innerWidth / 2 + window.innerWidth / 4, window.innerHeight / 2);
+		door.name = "doorOpened";
 
 		app.stage.removeChild(handleContainer);
 		secretWheels.forEach(value => app.stage.removeChild(value));
 
 		app.stage.addChild(endScreen);
-	} else {
-		for (let j = 0; j < keyComboCount; j++) {
-			secretWheels[j].children[0].rotation = 0;
-			secretWheelCombo[j].secretKey = 1;
-			secretWheelCombo[j].direction = KeyDirections.Left;
+		app.ticker.remove(trackTime);
 
-			let shieldSprite = secretWheels[j].children[1] as Sprite;
-			shieldSprite.texture = Assets.get("wheelShield");
-		}
-		keyComboAnswer = CreateRandomKeyCombo(keyComboCount);
+		app.ticker.add(resetAfter);
+	} else {
+		resetVault();
 	}
 }
 
-Assets.add("background", "bg.png");
-Assets.add("doorClosed", "door.png");
-Assets.add("doorOpened", "doorOpen.png");
-Assets.add("handle", "handle.png");
-Assets.add("handleShadow", "handleShadow.png");
-Assets.add("wheelShield","wheelShield.png");
-Assets.add("wheelShieldLeft","wheelShieldLeft.png");
-Assets.add("wheelShieldRight","wheelShieldRight.png");
-Assets.add("enterCodeButton", "enterCode.png");
-Assets.add("glitter", "blink.png");
+function resetVault() {
+	for (let j = 0; j < keyComboCount; j++) {
+		//secretWheels[j].children[0].rotation = 0;
+		secretWheelCombo[j].secretKey = 1;
+		secretWheelCombo[j].direction = KeyDirections.Left;
+		secretWheelCombo[j].rotation = 0;
+		if(secretWheelCombo[j].tweenAnim != undefined) {
+			// @ts-ignore
+			secretWheelCombo[j].tweenAnim.kill()
+		}
+
+		secretWheelCombo[j].tweenAnim = gsap.to(secretWheels[j].children[0] as PIXI.Sprite, {pixi:{rotation: 0}, duration: 1});
+
+		let shieldSprite = secretWheels[j].children[1] as PIXI.Sprite;
+		shieldSprite.texture = PIXI.Assets.get("wheelShield");
+	}
+	keyComboAnswer = CreateRandomKeyCombo(keyComboCount);
+	gsap.to(handleSprite, {pixi: {rotation: 999}, duration: 1});
+	app.ticker.add(trackTime);
+	timer = 0;
+}
+
+async function resetAfter() {
+	endGameTimeoutSeconds += app.ticker.deltaMS / 1000;
+	if (endGameTimeoutSeconds >= 5) {
+		let door = backgroundContainer.children.find(value => value.name === "doorOpened") as PIXI.Sprite;
+		door.texture = await PIXI.Assets.load("doorClosed");
+		door.position.set(window.innerWidth / 2 + 28, window.innerHeight / 2 - 12);
+		door.name = "doorClosed";
+
+		app.stage.addChild(handleContainer);
+		secretWheels.forEach(value => app.stage.addChild(value));
+
+		app.stage.removeChild(endScreen);
+
+		app.ticker.remove(resetAfter);
+		resetVault();
+		endGameTimeoutSeconds = 0;
+	}
+}
+
+function trackTime() {
+	timer += app.ticker.elapsedMS / 1000;
+	time.text = new Date(timer * 1000).toISOString().slice(11, 19);
+}
+
+PIXI.Assets.add("background", "bg.png");
+PIXI.Assets.add("doorClosed", "door.png");
+PIXI.Assets.add("doorOpened", "doorOpen.png");
+PIXI.Assets.add("handle", "handle.png");
+PIXI.Assets.add("handleShadow", "handleShadow.png");
+PIXI.Assets.add("wheelShield","wheelShield.png");
+PIXI.Assets.add("wheelShieldLeft","wheelShieldLeft.png");
+PIXI.Assets.add("wheelShieldRight","wheelShieldRight.png");
+PIXI.Assets.add("enterCodeButton", "enterCode.png");
+PIXI.Assets.add("glitter", "blink.png");
 
 
-Assets.load(["background", "doorClosed", "handle", "handleShadow", "enterCodeButton", "glitter"]).then(bgTexture => {
+PIXI.Assets.load(["background", "doorClosed", "handle", "handleShadow", "enterCodeButton", "glitter"]).then(bgTexture => {
 	loadImageToContainer(bgTexture.background, 0, 0, 1, backgroundContainer);
 	loadImageToContainer(bgTexture.doorClosed, 28, -12, 3, backgroundContainer);
 	handleSprite = loadImageToContainer(bgTexture.handleShadow, 0, 0, 9, handleContainer);
@@ -229,11 +282,11 @@ Assets.load(["background", "doorClosed", "handle", "handleShadow", "enterCodeBut
 
 	handleSpriteReal.position.set(-10,-12);
 
-	handleSprite.hitArea = new Rectangle(-window.innerWidth / 2,-window.innerHeight / 2, window.innerWidth, window.innerHeight);
+	handleSprite.hitArea = new PIXI.Rectangle(-window.innerWidth / 2,-window.innerHeight / 2, window.innerWidth, window.innerHeight);
 	handleSprite.interactive = true;
 	handleSprite.on('pointermove', rotateHandle);
 
-	let buttonSprite: Sprite = Sprite.from(bgTexture.enterCodeButton);
+	let buttonSprite: PIXI.Sprite = PIXI.Sprite.from(bgTexture.enterCodeButton);
 	buttonSprite.position.set(window.innerWidth / 2 + window.innerWidth / 4,window.innerHeight / 2);
 	buttonSprite.anchor.set(0.5,0.5);
 	buttonSprite.interactive = true;
@@ -246,7 +299,7 @@ Assets.load(["background", "doorClosed", "handle", "handleShadow", "enterCodeBut
 	loadImageToContainer(bgTexture.glitter, -120, 160 ,10, endScreen );
 	loadImageToContainer(bgTexture.glitter, 160, 80 ,10, endScreen );
 
-	let endText = new Text("You WIN!!", new TextStyle({
+	let endText = new PIXI.Text("You WIN!!", new PIXI.TextStyle({
 		fontFamily: 'Arial',
 		fontSize: 128,
 		fontStyle: 'italic',
@@ -270,9 +323,9 @@ Assets.load(["background", "doorClosed", "handle", "handleShadow", "enterCodeBut
 	//console.log(backgroundContainer);
 });
 
-Assets.load(["wheelShield", "wheelShieldLeft", "wheelShieldRight"]).then(texture => {
+PIXI.Assets.load(["wheelShield", "wheelShieldLeft", "wheelShieldRight"]).then(texture => {
 	for (let i = -1; i < 2; i++) {
-		let wheelContainer = new Container();
+		let wheelContainer = new PIXI.Container();
 
 		let positionx = window.innerWidth / 2 + (window.innerWidth / 4 * i) ;
 		let positiony = window.innerHeight / 2 - window.innerHeight / 3;
@@ -287,7 +340,7 @@ Assets.load(["wheelShield", "wheelShieldLeft", "wheelShieldRight"]).then(texture
 			wheelIndex = (i + 1);
 		})
 
-		let wheelShield: Sprite = Sprite.from(texture.wheelShield);
+		let wheelShield: PIXI.Sprite = PIXI.Sprite.from(texture.wheelShield);
 		wheelShield.position.set(positionx, positiony);
 		wheelShield.anchor.set(0.5, 0.5);
 		//wheelShield.position.set(positionx,positiony);
@@ -304,3 +357,9 @@ Assets.load(["wheelShield", "wheelShieldLeft", "wheelShieldRight"]).then(texture
 
 	secretWheels.forEach(value => app.stage.addChild(value))
 });
+
+const time = new PIXI.Text(0, style);
+time.position.set(window.innerWidth / 2 - window.innerWidth / 5, window.innerHeight / 2 - window.innerHeight / 17);
+time.anchor.set(0.5,0.5);
+app.ticker.add(trackTime);
+app.stage.addChild(time);
